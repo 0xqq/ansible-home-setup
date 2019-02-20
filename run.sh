d10ecdb39497a8fbe1adb9fcce8c134abfcd6f1b
Jiri Dokoupil <33521168+jiridoko@users.noreply.github.com>
2019-02-20 19:18:43 +0000

    updates

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-ansible-playbook -i inventory --vault-id @prompt playbook.yml
+if [ -z "$1" ]; then
+	ansible-playbook -i inventory --vault-id @prompt playbook.yml
+elif [ "$1" == "edit" ]; then
+	EDITOR=vim ansible-vault --ask-vault-pass edit group_vars/all.yml
+fi
+
