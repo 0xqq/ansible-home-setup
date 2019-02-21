b588b489e09916371e51393446f41b1441b2be3d
Jiří Dokoupil <33521168+jiridoko@users.noreply.github.com>
2019-02-21 00:16:37 +0000

    adding the vagrant keys

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 if [ -z "$1" ]; then
 	ansible-playbook -i inventory --vault-password-file /vagrant_home/.vaultp playbook.yml
 elif [ "$1" == "edit" ]; then
-	EDITOR=vim ansible-vault --ask-vault-pass edit group_vars/all.yml
+	#EDITOR=vim ansible-vault --ask-vault-pass edit group_vars/all.yml
+	EDITOR=vim ansible-vault --vault-password-file /vagrant_home/.vaultp edit group_vars/all.yml
 elif [ "$1" == "dns" ]; then
 	ansible-playbook -i inventory --vault-password-file /vagrant_home/.vaultp playbook.yml -t zone
 fi
