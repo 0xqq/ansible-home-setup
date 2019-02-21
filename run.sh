9d05acfa409d97aeed188dec5a02e75964ca9ec9
Jiri Dokoupil <33521168+jiridoko@users.noreply.github.com>
2019-02-21 00:00:00 +0000

    using the password file for regular runs also

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	ansible-playbook -i inventory --vault-id @prompt playbook.yml
+	ansible-playbook -i inventory --vault-password-file /vagrant_home/.vaultp playbook.yml
 elif [ "$1" == "edit" ]; then
 	EDITOR=vim ansible-vault --ask-vault-pass edit group_vars/all.yml
 elif [ "$1" == "dns" ]; then
-	#ansible-playbook -i inventory --vault-id @prompt playbook.yml -t zone
 	ansible-playbook -i inventory --vault-password-file /vagrant_home/.vaultp playbook.yml -t zone
 fi
 
