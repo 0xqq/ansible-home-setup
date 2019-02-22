fa5414862e2e99b32aea9cfa6211d553cb7d7e10
Jiri Dokoupil <33521168+jiridoko@users.noreply.github.com>
2019-02-22 15:50:29 +0000

    installing openvpn and more

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	ansible-playbook -i inventory --vault-password-file /vagrant_home/.vaultp playbook.yml
+	ansible-playbook -i inventory --vault-password-file ~/.vaultp playbook.yml
 elif [ "$1" == "edit" ]; then
 	#EDITOR=vim ansible-vault --ask-vault-pass edit group_vars/all.yml
-	EDITOR=vim ansible-vault --vault-password-file /vagrant_home/.vaultp edit group_vars/all.yml
+	EDITOR=vim ansible-vault --vault-password-file ~/.vaultp edit group_vars/all.yml
 elif [ "$1" == "dns" ]; then
-	ansible-playbook -i inventory --vault-password-file /vagrant_home/.vaultp playbook.yml -t zone
+	ansible-playbook -i inventory --vault-password-file ~/.vaultp playbook.yml -t zone
 fi
 
